07731f38d8e82c8e3395e9544480cad9e1970f99
Axana <kseshechka1996@gmail.com>
2023-08-29 14:11:21 +0300

    tab-54

---
+++ tab-54/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 }
+        };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        SortRowsDescending(array);
+
+        Console.WriteLine("Массив после упорядочивания по убыванию элементов каждой строки:");
+        PrintArray(array);
+
+        Console.ReadLine();
+    }
+
+    static void SortRowsDescending(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns - 1; j++)
+            {
+                for (int k = 0; k < columns - j - 1; k++)
+                {
+                    if (array[i, k] < array[i, k + 1])
+                    {
+                        int temp = array[i, k];
+                        array[i, k] = array[i, k + 1];
+                        array[i, k + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
